a0e42cfc2a4da45d4debb1e0cdcff8516a489ebb
Wiktor <wiktormax2@gmail.com>
2020-03-22 14:20:01 +0100

    replace files

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\wiktor\desktop\projects\portfo~1\discov~1\backend\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
