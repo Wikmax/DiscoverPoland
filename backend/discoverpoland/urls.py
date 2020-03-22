2ebdcefba0634d3a039d0642c9c79faaad47fed9
Wiktor <wiktormax2@gmail.com>
2020-03-22 19:14:59 +0100

    Update urls.py

--- backend/discoverpoland/urls.py
+++ backend/discoverpoland/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
@@ -8,5 +8,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', admin.site.urls),
     path('api/', include('points.api.urls')),
-     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
