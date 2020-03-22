844af68139f8328614b4780bb890e14465fcf3e7
Wiktor <wiktormax2@gmail.com>
2020-03-22 14:28:46 +0100

    Update wsgi.py

--- backend/discoverpoland/wsgi.py
+++ backend/discoverpoland/wsgi.py
@@ -10,13 +10,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djreact.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discoverpoland.settings')
-
-application = get_wsgi_application()
