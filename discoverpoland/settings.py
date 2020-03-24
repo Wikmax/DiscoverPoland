2aa4283de3dbeca575a48d3ff99dd06eed5bded5
Wiktor <wiktormax2@gmail.com>
2020-03-24 12:43:40 +0100

    media_root

--- discoverpoland/settings.py
+++ discoverpoland/settings.py
@@ -102,7 +102,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'src')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), '')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
