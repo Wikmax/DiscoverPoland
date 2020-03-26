5da85fbc20bf7515b118493f4de684674de16250
Wiktor <wiktormax2@gmail.com>
2020-03-26 14:08:01 +0100

    Update urls.py

--- discoverpoland/urls.py
+++ discoverpoland/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('points.api.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
