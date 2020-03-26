0b3262fb59e5282c034c85ddf91d38f71bd0cbb6
Wiktor <wiktormax2@gmail.com>
2020-03-26 14:34:44 +0100

    Update urls.py

--- discoverpoland/urls.py
+++ discoverpoland/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 from django.views.generic import TemplateView
 from django.views.static import serve 
 
