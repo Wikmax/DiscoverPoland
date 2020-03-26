7947b44d6401a9f7cb2bac0c5a67350ade6c1a30
Wiktor <wiktormax2@gmail.com>
2020-03-26 14:20:08 +0100

    Update urls.py

--- discoverpoland/urls.py
+++ discoverpoland/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include, re_path, url
 from django.conf import settings
+from django.conf.urls import url, patterns
 from django.views.generic import TemplateView
 from django.views.static import serve 
 
@@ -8,6 +9,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('points.api.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
